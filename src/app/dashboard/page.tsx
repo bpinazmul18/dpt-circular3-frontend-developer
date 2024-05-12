ed6465adb0215b1bb7436d8fabc67a66100178f4
Nazmul Haque <bpinazmul18@gmail.com>
2024-05-13 02:18:11 +0600

    chore: remove unnecessary

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,3 @@
-import { headers } from "next/headers";
-import SecondaryHeader from "@/components/SecondaryHeader";
 export default function Dashboard() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-2"></main>
-  );
+  return <></>;
 }
